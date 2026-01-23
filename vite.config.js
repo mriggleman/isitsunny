1e7f77930e48e378756a17b8bf8b7144b8163229
mriggleman <119722213+mriggleman@users.noreply.github.com>
2026-01-23 23:48:27 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/isitsunny/'
 })
-```
-
-**.gitignore**:
-```
-node_modules
-dist
-.DS_Store
