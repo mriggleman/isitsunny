2af9c05dfd4ac3699a6bb99ee2482557c205513b
mriggleman <119722213+mriggleman@users.noreply.github.com>
2026-01-23 23:50:47 +0000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
+
+**src/App.jsx**
+(Copy the entire React component code I provided earlier here)
+
+**.gitignore**
+```
+node_modules
+dist
+.DS_Store
